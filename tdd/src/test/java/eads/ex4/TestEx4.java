e5f2b9773a91d2456a2c9cc31242debe1cd170d8
kpi-jps <jps.spj@gmail.com>
2023-09-21 11:51:54 -0300

    feat: added all unit tests to Ex4 class

--- tdd/src/test/java/eads/ex4/TestEx4.java
+++ tdd/src/test/java/eads/ex4/TestEx4.java
@@ -16,6 +16,27 @@ public class TestEx4 {
         assertThat(result).isEqualTo(0);
     }
 
-    
+    @Test
+    @DisplayName ("Should give the minimal number of actioned switchers to different initial and final state for A and equal initial and final state for B")
+    void shouldGivenTheMinimalNUmberOfActionedSwitchersToDifferenInitialAndFinalStateForAAndEqualInitialAndFinalStateForB() {
+        int result = sut.getTheNumberOfSwitchersClicked(1, 0, 0, 0);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName ("Should give the minimal number of actioned switchers to equal initial and final state for A and different initial and final state for B")
+    void shouldGivenTheMinimalNUmberOfActionedSwitchersToEqualInitialAndFinalStateForAAndDifferentInitialAndFinalStateForB() {
+        int result = sut.getTheNumberOfSwitchersClicked(1, 1, 1, 0);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName ("Should give the minimal number of actioned switchers to different initial and final state for A and different initial and final state for B")
+    void shouldGivenTheMinimalNUmberOfActionedSwitchersToDifferentInitialAndFinalStateForAAndDifferentInitialAndFinalStateForB() {
+        int result = sut.getTheNumberOfSwitchersClicked(0, 0, 1, 1);
+        assertThat(result).isEqualTo(1);
+    }
+
+
 
 }
